548b33feeaa0e4a2a892d721aafb2c3c37006e99
luzhenting <luzhenting@anji-allways.com>
2019-04-22 14:17:24 +0800

    config-service

--- consumer-feign/src/main/java/cn/ppl/ConsumerFeignApplication.java
+++ consumer-feign/src/main/java/cn/ppl/ConsumerFeignApplication.java
@@ -1,16 +1,20 @@
 package cn.ppl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 public class ConsumerFeignApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerFeignApplication.class, args);
 	}
